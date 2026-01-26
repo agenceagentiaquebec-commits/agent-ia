d1e05ec5aa5d85fb88e9c0fa44c568e7793bb9b5
CHAYED Nawfel <nawfel.chayed@keolis.ca>
2026-01-26 00:53:01 -0500

    Change url

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def voice(request: Request):
         )
         return Response(
             content=f"""<Response>
-<Play>https://emily-backend-zilmjqw47q-nn.a.run.app/voice-file</Play>
+<Play>https://emily-backend-996818120694.northamerica-northeast1.run.app/voice-file</Play>
 <Pause length="1"/>
 <Redirect>/listen</Redirect>
 </Response>""",
@@ -151,7 +151,7 @@ async def voice(request: Request):
     # Emily parle -> puis Twilio écoute
     return Response(
         content=f"""<Response>
-<Play>https://emily-backend-zilmjqw47q-nn.a.run.app/voice-file</Play>
+<Play>https://emily-backend-996818120694.northamerica-northeast1.run.app/voice-file</Play>
 <Pause length="1"/>
 <Redirect>/listen</Redirect>
 </Response>""",
